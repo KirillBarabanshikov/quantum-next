67ba987db7dc0c2bde0a8a6fe6ec59ce86820efe
Kirill <kirillbar24@gmail.com>
2024-09-12 14:34:35 +0400

    added router push

--- src/widgets/CategoriesList/CategoriesList.tsx
+++ src/widgets/CategoriesList/CategoriesList.tsx
@@ -1,16 +1,21 @@
+'use client';
+
 import clsx from 'clsx';
+import { useRouter } from 'next/navigation';
 import { FC } from 'react';
 
 import { CategoryCard } from '@/entities/category';
+import { Button } from '@/shared/ui';
 
 import styles from './CategoriesList.module.scss';
-import { Button } from '@/shared/ui';
 
 interface ICategoriesListProps {
     className?: string;
 }
 
 export const CategoriesList: FC<ICategoriesListProps> = ({ className }) => {
+    const router = useRouter();
+
     return (
         <div className={clsx(className)}>
             <div className={'container'}>
@@ -19,7 +24,7 @@ export const CategoriesList: FC<ICategoriesListProps> = ({ className }) => {
                         <CategoryCard key={index} />
                     ))}
                 </div>
-                <Button variant={'outline'} className={styles.button}>
+                <Button variant={'outline'} onClick={() => router.push('/catalog')} className={styles.button}>
                     Каталог
                 </Button>
             </div>
