e09aa045f7ca2ca5dc67c3b8f38edb004d3865ac
Kirill <kirillbar24@gmail.com>
2024-09-13 12:31:10 +0400

    removed buttons for ProductsCarousel.tsx

--- src/widgets/ProductsCarousel/ProductsCarousel.tsx
+++ src/widgets/ProductsCarousel/ProductsCarousel.tsx
@@ -1,13 +1,10 @@
 'use client';
 
 import clsx from 'clsx';
-import { FC, useState } from 'react';
-import SwiperCore from 'swiper';
+import { FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { ProductCard } from '@/entities/product';
-import ArrowBackwardIcon from '@/shared/assets/icons/arrow_backward.svg';
-import ArrowForwardIcon from '@/shared/assets/icons/arrow_forward.svg';
 
 import styles from './ProductsCarousel.module.scss';
 
@@ -17,28 +14,28 @@ interface IProductsCarouselProps {
 }
 
 export const ProductsCarousel: FC<IProductsCarouselProps> = ({ title, className }) => {
-    const [swiper, setSwiper] = useState<SwiperCore | null>(null);
+    // const [swiper, setSwiper] = useState<SwiperCore | null>(null);
 
     return (
         <section className={clsx(styles.productsCarouselWrap, className)}>
             <div className={'container'}>
                 <div className={styles.titleWrap}>
                     <h2 className={'title'}>{title}</h2>
-                    <div className={styles.buttons}>
-                        <button onClick={() => swiper?.slidePrev()}>
-                            <ArrowBackwardIcon />
-                        </button>
-                        <button onClick={() => swiper?.slideNext()}>
-                            <ArrowForwardIcon />
-                        </button>
-                    </div>
+                    {/*<div className={styles.buttons}>*/}
+                    {/*    <button onClick={() => swiper?.slidePrev()}>*/}
+                    {/*        <ArrowBackwardIcon />*/}
+                    {/*    </button>*/}
+                    {/*    <button onClick={() => swiper?.slideNext()}>*/}
+                    {/*        <ArrowForwardIcon />*/}
+                    {/*    </button>*/}
+                    {/*</div>*/}
                 </div>
             </div>
 
             <Swiper
                 slidesPerView={'auto'}
                 breakpoints={{ 0: { spaceBetween: 11 }, 768: { spaceBetween: 20 } }}
-                onSwiper={setSwiper}
+                // onSwiper={setSwiper}
                 className={'container'}
             >
                 {Array.from({ length: 10 }).map((_, index) => {
