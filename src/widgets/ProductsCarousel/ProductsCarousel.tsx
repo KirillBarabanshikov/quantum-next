0710aa28a5bcc2a285f1e8477dc7f35ede3a2d7f
Kirill <kirillbar24@gmail.com>
2024-09-21 15:16:20 +0400

    fix

--- src/widgets/ProductsCarousel/ProductsCarousel.tsx
+++ src/widgets/ProductsCarousel/ProductsCarousel.tsx
@@ -14,28 +14,17 @@ interface IProductsCarouselProps {
 }
 
 export const ProductsCarousel: FC<IProductsCarouselProps> = ({ title, className }) => {
-    // const [swiper, setSwiper] = useState<SwiperCore | null>(null);
-
     return (
         <section className={clsx(styles.productsCarouselWrap, className)}>
             <div className={'container'}>
                 <div className={styles.titleWrap}>
                     <h2 className={'title'}>{title}</h2>
-                    {/*<div className={styles.buttons}>*/}
-                    {/*    <button onClick={() => swiper?.slidePrev()}>*/}
-                    {/*        <ArrowBackwardIcon />*/}
-                    {/*    </button>*/}
-                    {/*    <button onClick={() => swiper?.slideNext()}>*/}
-                    {/*        <ArrowForwardIcon />*/}
-                    {/*    </button>*/}
-                    {/*</div>*/}
                 </div>
             </div>
 
             <Swiper
                 slidesPerView={'auto'}
                 breakpoints={{ 0: { spaceBetween: 11 }, 768: { spaceBetween: 20 } }}
-                // onSwiper={setSwiper}
                 className={'container'}
             >
                 {Array.from({ length: 10 }).map((_, index) => {
