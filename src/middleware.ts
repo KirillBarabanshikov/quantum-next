5df4e3cdf76e34af4e23c370adcb808f7a5ad362
Kirill <kirillbar24@gmail.com>
2024-09-22 00:35:20 +0400

    fix middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,13 @@ import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(req: NextRequest) {
     const token = req.cookies.get('token');
-    // const test = req.cookies.get('test');
+    const test = req.cookies.get('test');
     const { pathname } = req.nextUrl;
 
-    // if (!test && pathname !== '/loign') {
-    //     const url = new URL('/login', req.url);
-    //     return NextResponse.redirect(url);
-    // }
+    if (!test && pathname !== '/loign') {
+        const url = new URL('/login', req.url);
+        return NextResponse.redirect(url);
+    }
 
     if (!token && (pathname.startsWith('/cabinet') || pathname.startsWith('/create-profile'))) {
         const url = new URL('/?authentication=signin', req.url);
