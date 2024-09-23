b8bac32212e051de1a148ebed1676e74158fc9de
Kirill <kirillbar24@gmail.com>
2024-09-23 14:15:58 +0400

    fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,25 @@
 'use client';
 
+import { useMutation } from '@tanstack/react-query';
 import clsx from 'clsx';
 import Cookies from 'js-cookie';
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react';
 
+import { instance } from '@/shared/api';
+import Logo from '@/shared/assets/logos/logo_dark.svg';
 import { Button, Input } from '@/shared/ui';
 
 import styles from './page.module.scss';
 
 export default function Page() {
     const [value, setValue] = useState('');
+    const [email, setEmail] = useState('');
     const router = useRouter();
+
+    const { mutateAsync } = useMutation({
+        mutationFn: async (email: string) => await instance.post('/forms', { email }),
+    });
 
     const onSubmit = () => {
         if (value === 'UAVmarkt24') {
@@ -20,20 +28,31 @@ export default function Page() {
         }
     };
 
+    const onEmailSubmit = async () => {
+        if (!email) return;
+
+        try {
+            await mutateAsync(email);
+            setEmail('');
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
     return (
         <div className={clsx(styles.container)}>
-            <h1 className={'title'}>Сайт в разработке</h1>
-            <br />
-            <br />
-            <br />
-            <br />
+            <div className={styles.title}>
+                <Logo />
+                <h1 className={'title'}>Сайт в разработке</h1>
+            </div>
             <div style={{ width: '400px' }}>
                 <p>Оставьте ваш адрес электронной почты, и мы сообщим вам о запуске маркетплейса Rubot.pro</p>
                 <br />
-                <Input placeholder={'Email'} type={'email'} extent={'md'} />
+                <Input placeholder={'Email'} type={'email'} extent={'md'} onChange={(e) => setEmail(e.target.value)} />
                 <br />
-                <Button fullWidth>Отправить</Button>
-                <br />
+                <Button fullWidth onClick={() => onEmailSubmit()}>
+                    Отправить
+                </Button>
                 <br />
                 <br />
                 <br />
