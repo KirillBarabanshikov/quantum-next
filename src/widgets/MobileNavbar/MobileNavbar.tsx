08a95a5107917733bca0386a63c5ae8036ef6ab6
Kirill <kirillbar24@gmail.com>
2024-10-23 17:15:04 +0400

    fix mobile menu

--- src/widgets/MobileNavbar/MobileNavbar.tsx
+++ src/widgets/MobileNavbar/MobileNavbar.tsx
@@ -8,16 +8,17 @@ import AccountIcon from '@/shared/assets/icons/account.svg';
 import BagIcon from '@/shared/assets/icons/cart.svg';
 import CatalogIcon from '@/shared/assets/icons/catalog.svg';
 import HomeIcon from '@/shared/assets/icons/home.svg';
+import SearchIcon from '@/shared/assets/icons/search.svg';
 import GradeIcon from '@/shared/assets/icons/star.svg';
 
 import styles from './MobileNavbar.module.scss';
 
 const navItems = [
-    { path: '/', icon: <HomeIcon /> },
-    { path: '/catalog', icon: <CatalogIcon /> },
-    { path: '/cart', icon: <BagIcon /> },
-    { path: '/favorites', icon: <GradeIcon /> },
-    { path: '/cabinet', icon: <AccountIcon /> },
+    { path: '/', icon: <HomeIcon />, title: 'Главная' },
+    { path: '/catalog', icon: <CatalogIcon />, title: 'Каталог' },
+    { path: '/cart', icon: <BagIcon />, title: 'Корзина' },
+    { path: '/favorites', icon: <GradeIcon />, title: 'Избранное' },
+    { path: '/cabinet', icon: <AccountIcon />, title: 'Профиль' },
 ] as const;
 
 export const MobileNavbar = () => {
@@ -26,14 +27,28 @@ export const MobileNavbar = () => {
     return (
         <nav className={styles.mobileNavbar}>
             {navItems.map((item, index) => {
+                if (item.path === '/catalog' && pathname === '/') {
+                    return (
+                        <div key={index} className={clsx(styles.mobileNavbarItem)}>
+                            <div className={styles.icon}>
+                                <SearchIcon />
+                            </div>
+                            <div className={styles.title}>Поиск</div>
+                        </div>
+                    );
+                }
+
                 return (
                     <Link
                         key={index}
                         href={item.path}
-                        className={clsx(styles.bottomNavbarItem, pathname === item.path && styles.active)}
+                        className={clsx(styles.mobileNavbarItem, pathname === item.path && styles.active)}
                     >
-                        {item.icon}
-                        {item.path === '/cart' && <span className={styles.badge}>0</span>}
+                        <div className={styles.icon}>
+                            {item.icon}
+                            {item.path === '/cart' && <span className={styles.badge}>0</span>}
+                        </div>
+                        <div className={styles.title}>{item.title}</div>
                     </Link>
                 );
             })}
