d3e8d5710f08af1b11911123a5cefa51f9b6e94b
Kirill <kirillbar24@gmail.com>
2024-11-06 17:04:33 +0400

    fix ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,10 @@ module.exports = {
     apps: [
         {
             name: 'rubot',
+            exec_mode: 'cluster',
+            instances: 'max',
             script: 'node_modules/next/dist/bin/next',
+            args: 'start',
         },
     ],
 };
