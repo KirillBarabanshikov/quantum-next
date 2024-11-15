e8a20ae0be786e24e04a5563bb30cc0f8b8197d0
Kirill <93821977+KirillBarabanshikov@users.noreply.github.com>
2024-11-15 14:03:54 +0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'dc-mining-next',
+            name: 'rubot',
             exec_mode: 'cluster',
             instances: 2,
             script: 'node_modules/next/dist/bin/next',
