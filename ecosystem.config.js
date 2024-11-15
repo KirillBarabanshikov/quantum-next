68f6af61b532e8ffde842961f6e5d7e156069e19
Kirill <kirillbar24@gmail.com>
2024-11-15 13:54:55 +0400

    fix ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,12 @@
 module.exports = {
     apps: [
         {
-            name: 'rubot',
+            name: 'dc-mining-next',
             exec_mode: 'cluster',
-            instances: 'max',
+            instances: 2,
             script: 'node_modules/next/dist/bin/next',
             args: 'start',
+            max_memory_restart: '500M',
         },
     ],
 };
